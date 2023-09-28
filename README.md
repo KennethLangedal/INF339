411d3d5cb905a7186f6eaf8c3085998138873ba8
KennethLangedal <kenneth.langedal@hotmail.com>
2023-09-28 11:59:41 +0200

    Individual timeings for column

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ srun -p rome16q -N 1 --pty bash --login
 module load gcc/11.2.0
 module load openmpi/gcc/64/4.0.1
 mpicc -O3 -march=native -ffast-math
+ssh -AY2C kennethl@dnat.simula.no -p 60441
 ```
--- gemv_column_time.c
+++ gemv_column_time.c
@@ -12,7 +12,7 @@ int main(int argc, char **argv)
     MPI_Comm_rank(MPI_COMM_WORLD, &rank); // get current process id
     MPI_Comm_size(MPI_COMM_WORLD, &size); // get number of processes
 
-    double bt = 1e9, btc1, btf, btc2;
+    double bt = 1e9, btc1 = 1e9, btf = 1e9, btc2 = 1e9;
 
     MPI_Barrier(MPI_COMM_WORLD);
     double ts0 = MPI_Wtime();
@@ -48,6 +48,9 @@ int main(int argc, char **argv)
     }
 
     double *b = (double *)aligned_alloc(32, sizeof(double) * N);
+    double *res;
+    if (rank == 0)
+        res = (double *)aligned_alloc(32, sizeof(double) * N);
 
     MPI_Barrier(MPI_COMM_WORLD);
     double ts1 = MPI_Wtime();
@@ -62,6 +65,13 @@ int main(int argc, char **argv)
         MPI_Barrier(MPI_COMM_WORLD);
         double t1 = MPI_Wtime();
 
+        if (rank == 0 && t1 - t0 < btc1)
+            btc1 = t1 - t0;
+    }
+    for (size_t it = 0; it < n_it; it++)
+    {
+        MPI_Barrier(MPI_COMM_WORLD);
+        double t0 = MPI_Wtime();
 #ifdef AVX
         for (size_t i = 0; i < N; i++)
         {
@@ -87,25 +97,20 @@ int main(int argc, char **argv)
 #endif
 
         MPI_Barrier(MPI_COMM_WORLD);
-        double t2 = MPI_Wtime();
-
-        if (rank == 0)
-            MPI_Reduce(MPI_IN_PLACE, b, N, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-        else
-            MPI_Reduce(b, b, N, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+        double t1 = MPI_Wtime();
+        if (rank == 0 && t1 - t0 < btf)
+            btf = t1 - t0;
+    }
+    for (size_t it = 0; it < n_it; it++)
+    {
         MPI_Barrier(MPI_COMM_WORLD);
-        double t3 = MPI_Wtime();
+        double t0 = MPI_Wtime();
+        MPI_Reduce(b, res, N, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+        MPI_Barrier(MPI_COMM_WORLD);
+        double t1 = MPI_Wtime();
 
-        if (rank == 0)
-        {
-            if (t3 - t0 < bt)
-            {
-                bt = t3 - t0;
-                btc1 = t1 - t0;
-                btf = t2 - t1;
-                btc2 = t3 - t2;
-            }
-        }
+        if (rank == 0 && t1 - t0 < btc2)
+            btc2 = t1 - t0;
     }
 
     if (rank == 0) // Validate results
@@ -116,9 +121,10 @@ int main(int argc, char **argv)
             double target = 0.0;
             for (size_t j = 0; j < N; j++)
                 target += j * (i + j);
-            error += (target - b[i]) * (target - b[i]);
+            error += (target - res[i]) * (target - res[i]);
         }
 
+        bt = btc1 + btf + btc2;
         printf("%lf %lf %lf %lf %lf %lf\n", bt, btc1, btf, btc2, ts1 - ts0, error);
     }
 
