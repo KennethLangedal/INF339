9b6c0d26565f0efc0d111a732f312a45ee88669c
KennethLangedal <kenneth.langedal@hotmail.com>
2023-09-30 14:40:09 +0200

    Updates

--- gemv_grid.c
+++ gemv_grid.c
@@ -48,33 +48,45 @@ int main(int argc, char **argv)
     int scale = atoi(argv[1]);
     int n_it = atoi(argv[2]);
 
+    int p = 1;
+    while (p * p < size)
+        p++;
+
+    if (p * p != size)
+        return 2;
+
+    int g_i = rank / p, g_j = rank % p;
+
+    MPI_Comm row_comm, col_comm;
+    MPI_Comm_split(MPI_COMM_WORLD, g_i, rank, &row_comm);
+    MPI_Comm_split(MPI_COMM_WORLD, g_j, rank, &col_comm);
+
     size_t N = 1 << scale;
-    size_t rows = N / size;
+    size_t grid_n = N / p;
 
     // -----Initialization start-----
     MPI_Barrier(MPI_COMM_WORLD);
     double ts0 = MPI_Wtime();
 
     // Construct local part of A
-    double *A = (double *)aligned_alloc(32, sizeof(double) * rows * N);
-    for (size_t i = 0; i < rows; i++)
-        for (size_t j = 0; j < N; j++)
-            A[i * N + j] = rank * rows + i + j;
+    double *A = (double *)aligned_alloc(32, sizeof(double) * grid_n * grid_n);
+    for (size_t i = 0; i < grid_n; i++)
+        for (size_t j = 0; j < grid_n; j++)
+            A[i * grid_n + j] = g_i * grid_n + i + g_j * grid_n + j;
 
-    // Allocate data for x, rank 0 initialize
-    double *x = (double *)aligned_alloc(32, sizeof(double) * N);
-    if (rank == 0)
+    // Allocate data for x, ranks along the diagonal initialize
+    double *x = (double *)aligned_alloc(32, sizeof(double) * grid_n);
+    if (g_i == g_j)
     {
-        for (size_t i = 0; i < N; i++)
-            x[i] = i;
+        for (size_t i = 0; i < grid_n; i++)
+            x[i] = g_j * grid_n + i;
     }
 
-    // Rank 0 allocates for the whole b array, everyone else just their share
-    double *b;
-    if (rank == 0)
-        b = (double *)aligned_alloc(32, sizeof(double) * N);
-    else
-        b = (double *)aligned_alloc(32, sizeof(double) * rows);
+    // Allocate data for b
+    double *b = (double *)aligned_alloc(32, sizeof(double) * grid_n);
+    double *res = NULL;
+    if (g_i == g_j)
+        res = (double *)aligned_alloc(32, sizeof(double) * grid_n);
 
     MPI_Barrier(MPI_COMM_WORLD);
     double ts1 = MPI_Wtime();
@@ -87,7 +99,7 @@ int main(int argc, char **argv)
         MPI_Barrier(MPI_COMM_WORLD);
         double t0 = MPI_Wtime();
 
-        MPI_Bcast(x, N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Bcast(x, grid_n, MPI_DOUBLE, g_j, col_comm);
 
 #ifdef TIME_INDIVIDUAL
         MPI_Barrier(MPI_COMM_WORLD);
@@ -116,12 +128,12 @@ int main(int argc, char **argv)
             b[i] = c[0] + c[1] + c[2] + c[3];
         }
 #else
-        for (size_t i = 0; i < rows; i++)
+        for (size_t i = 0; i < grid_n; i++)
         {
             b[i] = 0.0;
-            for (size_t j = 0; j < N; j++)
+            for (size_t j = 0; j < grid_n; j++)
             {
-                b[i] += A[i * N + j] * x[j];
+                b[i] += A[i * grid_n + j] * x[j];
             }
         }
 #endif
@@ -138,7 +150,7 @@ int main(int argc, char **argv)
         double t4 = MPI_Wtime();
 #endif
         // Gather results
-        MPI_Gather(b, rows, MPI_DOUBLE, b, rows, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Reduce(b, res, grid_n, MPI_DOUBLE, MPI_SUM, g_i, row_comm);
 
 #ifdef TIME_INDIVIDUAL
         MPI_Barrier(MPI_COMM_WORLD);
@@ -150,15 +162,21 @@ int main(int argc, char **argv)
 #endif
     }
 
-    if (rank == 0) // Validate results
+    if (rank == 0)
     {
+        double *final_res = (double *)aligned_alloc(32, sizeof(double) * N);
+        for (int i = 0; i < grid_n; i++)
+            final_res[i] = res[i];
+        for (int i = 1; i < p; i++)
+            MPI_Recv(final_res + i * grid_n, grid_n, MPI_DOUBLE, i * (p + 1), 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
         double error = 0.0;
         for (size_t i = 0; i < N; i++)
         {
             double target = 0.0;
             for (size_t j = 0; j < N; j++)
                 target += j * (i + j);
-            error += (target - b[i]) * (target - b[i]);
+            error += (target - final_res[i]) * (target - final_res[i]);
         }
 
 #ifdef TIME_INDIVIDUAL
@@ -169,6 +187,10 @@ int main(int argc, char **argv)
 #else
         printf("%lf %lf %lf %lf %lf\n", t.min, t.max, t.total / t.count, ts1 - ts0, error);
 #endif
+    }
+    else if (g_i == g_j)
+    {
+        MPI_Send(res, grid_n, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
     }
 
     free(b);
--- run.sh
+++ run.sh
@@ -1,23 +1,4 @@
 #!/bin/bash
-#SBATCH -p rome16q # partition (queue)
-#SBATCH -N 8 # number of nodes
-#SBATCH -o slurm.out
-#SBATCH -e slurm.err
-#SBATCH --exclusive
 
-ulimit -s 200240
-
-for ((i=1; i<=16; i*=2))
-do
-	srun --exclusive -n $i --ntasts-per-node $i ./a.out 15 20
-done
-
-for ((i=1; i<=8; i*=2))
-do
-	srun --exclusive -n $i --ntasts-per-node 1 ./a.out 15 20
-done
-
-for ((i=1; i<=8; i*=2))
-do
-	srun --exclusive -n $((i*16)) --ntasts-per-node 16 ./a.out 15 20
-done
+for ((i=1; i<=16; i++))
+	sbatch -N 1 --ntasks-per-node $i jobscript
